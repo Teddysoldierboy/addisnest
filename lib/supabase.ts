65230aa2cba13bc4a86347e1ad93dac07122ed72
Teddysoldierboy <tewodrosgizachew8@gmail.com>
2026-05-27 15:40:39 +0300

    fix supabase config

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(
-  process.env.NEXT_https://hoowlauzvmnipsdrcxkz.supabase.co/rest/v1/!,
-  process.env.NEXT_eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhvb3dsYXV6dm1uaXBzZHJjeGt6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1NzEyMzEsImV4cCI6MjA5NTE0NzIzMX0.g6UrHSJJWakwF8EI00Z-xc9EH_bSoHsNzzspBQ5D1iQ!
-)
+  supabaseUrl,
+  supabaseAnonKey
+);
